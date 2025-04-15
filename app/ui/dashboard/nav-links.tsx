c13b52aa3ccd100c99101eb48dad2426d1c0e03f
Elat Aymerick <el.emerick@gmail.com>
2025-04-15 14:54:38 +0300

    added streaming

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -13,7 +13,7 @@ import { usePathname } from 'next/navigation';
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
+    name: 'Invoicess',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
